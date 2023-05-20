137f22959fa47560096e71ce21c86c9e6f1be158
djscheper <dennisjeroenscheper@gmail.com>
2023-05-20 20:13:13 +0200

    Mutliple files are possible + pylint.

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -9,10 +9,10 @@ Usage:
 """
 
 import argparse as ap
-import numpy as np
-import multiprocessing as mp
 import csv
+import multiprocessing as mp
 import sys
+import numpy as np
 
 __author__ = "Dennis Scheper (373689)"
 __date__ = "18/05/2023"
@@ -24,15 +24,18 @@ def define_arguments() -> None:
     Defines arguments for command-line usage.
     """
     argparser = ap.ArgumentParser(
-        description="Script voor Opdracht 1 van Big Data Computing. Delivered by Dennis Scheper (373689).")
+        description="""Script voor Opdracht 1 van Big Data Computing.
+        Delivered by Dennis Scheper (373689).""")
     argparser.add_argument("-n", action="store",
                         dest="n", required=True, type=int,
                         help="Aantal cores om te gebruiken.")
-    argparser.add_argument("-o", action="store", dest="csvfile", type=ap.FileType('w', encoding='UTF-8'),
-                        required=False, help="CSV file om de output in op te slaan. Default is output naar terminal STDOUT")
-    argparser.add_argument("fastq_files", action="store", type=ap.FileType('r'), nargs='+', 
+    argparser.add_argument("-o", action="store", dest="csvfile",
+                           type=ap.FileType('w', encoding='UTF-8'),
+                           required=False, help="""CSV file om de output in op te slaan.
+                        Default is output naar terminal STDOUT""")
+    argparser.add_argument("fastq_files", action="store", type=ap.FileType('r'), nargs='+',
                            help="Minstens 1 Illumina Fastq Format file om te verwerken")
-    
+
     return argparser.parse_args()
 
 
@@ -43,20 +46,20 @@ def calculate_phred(fastqfile: str) -> list:
     return [ord(q) - 33 for q in fastqfile]
 
 
-def write_csv(csvfile, results: list, name) -> None:
+def write_csv(csvfile, results: list, name, multiple_files: bool) -> None:
     """
     Writes results to a csv file per given FASTQ file.
-    Name of resulting csv file will be formatted as:
+    Name of resulting csv file will be formatted as if there are multiple FastQ files:
         `[name of inputfile].[name of outputfile].csv`
     """
     header = ['base_nr', 'average_phred_score']
-    outputfile = f"{str(name.name)}.{str(csvfile.name)}"
-    with open(outputfile, 'w') as csv_output:
+    csvfile = f"{str(name.name)}.{str(csvfile.name)}" if multiple_files else csvfile.name
+    with open(csvfile, 'w', newline="", encoding='utf-8') as csv_output:
         csvwriter = csv.writer(csv_output)
         csvwriter.writerow(i for i in header)
 
-        for base_nmr, p in enumerate(results, start=1):
-            csvwriter.writerow([base_nmr, p])
+        for base_nmr, av_phred in enumerate(results, start=1):
+            csvwriter.writerow([base_nmr, av_phred])
 
 
 def main():
@@ -64,10 +67,10 @@ def main():
     Main function in which command-line arguments are parsed and functions executed.
     """
     args = define_arguments()
-    
+
     for file in args.fastq_files:
         try:
-            with open(file.name, 'r') as inputfile:
+            with open(file.name, 'r', encoding='utf-8') as inputfile:
                 lines = inputfile.read().splitlines() # remove \n
         except FileNotFoundError:
             print(f"Couldn't find {inputfile}")
@@ -78,9 +81,11 @@ def main():
             results = pool.map(calculate_phred, lines[3::4]) # only take the fourth line of read
 
         average = np.mean(results, axis=0)
-        
+
         if args.csvfile is not None:
-            write_csv(args.csvfile, average, file)
+            check_multiple = len(args.fastq_files)>1
+            write_csv(args.csvfile, average, file,
+                      multiple_files=check_multiple)
         else:
             print(f"Results for {file.name}")
             csv_writer = csv.writer(sys.stdout)
