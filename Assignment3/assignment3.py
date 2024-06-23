107e2996b180d2a14b3e023d8a3437fbe976667e
djscheper <d.j.scheper@st.hanze.nl>
2024-06-23 17:39:23 +0200

    Added a version of assignment 3 (probably not done yet)...

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -9,75 +9,107 @@ FIRST:
     chmod +x assignment3.sh
 
 Usage:
-    ./assignment3.sh <inputfiles>
+    ./assignment3.sh [inputfiles]
 """
 
 from collections import defaultdict
+import argparse as ap
 import sys
 import numpy as np
+import csv
+import json
+import time
 
 __author__ = "Dennis Scheper (373689)"
 __status__ = "Work in progress..."
-__date__ = ""
+__date__ = "23/06/2024"
 __contact__ = "d.j.scheper@st.hanze.nl"
 
-def calculation(file,chunck):
+def parse_arguments():
     """
-    Processes the FastQ file, calculates the Phred score, and adds it to a
-    defaultdict. Defaultdict allows us to add more base positions in a
-    straightforward manner.
-
-    args:
-    - chunck: chunck to be processed
-
-    returns:
-    - phred_scores: defaultdict with Phred score per base position for said chunck
+    Parses all arguments.
     """
-    # unpack the chunck's start and end positions
-    start, end = chunck
-    # defaultdict makes adding more base positions more flexible
+    parser = ap.ArgumentParser(description="Big data computing assignment 3 by Dennis Scheper")
+    parser.add_argument("--calculate", action="store_true", required=False,
+                        help="Generate chunks.")
+    parser.add_argument("--merge", action="store_true", required=False, help="Calculation option.")
+    parser.add_argument("fastq_files", action="store", nargs='*', help="Minstens 1 Illumina Fastq Format file om te verwerken")
+    parser.add_argument("-o", action="store", dest="csvfile", required=False, help="CSV file om de output in op te slaan. Default is output naar terminal STDOUT")
+    parser.add_argument("-n", type=int, required=False)
+    return parser.parse_args()
+
+def process_qline():
+    """
+    Processes the quality line of a given fastq file. Data comes in from a bash file and
+    here we calculate the quality per base position. Thereafter, we 'dump' the data via
+    json back to the bash script for further handling.
+    
+    Arguments: 
+      X
+      
+    Returns:
+      Prints the quality score per base position in json format
+    """
+    data = sys.stdin.buffer
     phred_scores = defaultdict(list)
+    line_num = 0
 
-    with open(file, 'rb') as inputfile:
-        inputfile.seek(start)
-        while inputfile.tell() < end:
-            line = inputfile.readline()
-            if line.startswith(b"@"):
-                inputfile.readline()
-                inputfile.readline()
-                qual = inputfile.readline().strip()
+    with data as input_data:
+        for line in input_data:
+            line_num += 1
+            if line_num % 4 == 0:
+                qual = line.strip()
                 for pos, quality in enumerate(qual):
-                    phred_scores[pos].append(quality - 33)  # no need for ord() since we are already working in bytes
+                    phred_scores[pos].append(quality - 33)
 
-    return phred_scores
+    print(json.dumps(dict(phred_scores)))
 
-def calculate_average(phred_scores):
+
+def calculate_average(filename, *, outputfile="output.csv", multiple=False):
     """
-    Calculates the average Phred score per base position by first merging
-    every dict per chunck into one. Then simply calculate the average.
-
-    args:
-    - phred_scores: list of defaultdicts 
-
-    returns:
-    - a defaultdict with the average phred score per base position
+    Calculates the average quality score per base position. Handles the results
+    by writing it to a CSV or prints it back to the command line.
+    
+    Arguments:
+      - filename: name of the fastq file
+      - outputfile: name output file (CSV)
+      - multiple: boolean whether determine if multiple fastq files need to be handled
+      
+    Returns:
+      X
     """
+    # receive the printed json from the process_qline function
+    phred_scores = json.loads(sys.stdin.read().strip())
     combined = defaultdict(list)
 
-    # combine every chunck into one dict
-    for sub_dict in phred_scores:
-        for key, value in sub_dict.items():
-            combined[key].extend(value)
-        
-    # then simply return the mean per base position
-    return {key: np.mean(values) for key, values in combined.items()}
+    for key, value in phred_scores.items():
+        combined[key].extend(value)
+
+    #calculate the averages per base position
+    averages = {key: np.mean(values) for key, values in phred_scores.items()}
+
+    name_output = outputfile if not multiple else f"{filename}.{outputfile}"
+    if filename is not None:
+        with open(name_output, 'w', newline='', encoding='UTF-8') as csv_file:
+            writer = csv.writer(csv_file)
+            for key, value in averages.items():
+                writer.writerow([key, value])
+    else: 
+        for key, value in averages.items():
+            print(f"{key}, {value}")
 
 
 def main():
     """
+    The main function; directs all functionality.
     """
-    input_data = sys.stdin.read()
-    print(input_data)
+    args = parse_arguments()
+    if args.calculate:
+        process_qline()
+    if args.merge:
+        filename = args.csvfile if args.csvfile else None
+        multiple = args.n > 1
+        calculate_average(filename=filename, multiple=multiple)
 
 if __name__ == "__main__":
     main()
--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-#tmp - easy to work with
+##
+# Created by Dennis Scheper - d.j.scheper@st.hanze.nl
+# Usage: 
+#       first time only; chmod +x assignment3.sh
+#       ./assignment3.sh [fastq_file1] [fastq_file2] [fastq_fileN]
+##
+
+# user can specify their own fastq file, if not given use the standard one
 if [ -z "$1" ]; then 
-    INPUT_FILE=~/Desktop/BDC/Assignment2/rnaseq_selection.fastq
+   INPUT_FILES="/commons/Themas/Thema12/HPC/rnaseq.fastq"
 else 
-    INPUT_FILE=$1
+   INPUT_FILES=("$@") # user input
 fi
 
-#https://www.gnu.org/software/parallel/parallel_tutorial.html
+#count how many files if user decides to give more than one
+NUM_FILES=${#INPUT_FILES[@]}
+HOSTS="nuc100,nuc101"
 
-cat "$INPUT_FILE" | parallel --pipe -j 3 python assignment3.py
+#export for parallel processes
+export WORK_DIR=$(realpath "$(dirname "$0")")
+
+for INPUT_FILE in "${INPUT_FILES[@]}"; do
+  export FILE_NAME=$(basename "$INPUT_FILE")
+  parallel -S "${HOSTS}" --pipepart --jobs 4 --block 500M --memfree 1G --recstart "@.*" python3 "${WORK_DIR}"/assignment3.py --calculate :::: "${INPUT_FILE}" | python3 "${WORK_DIR}"/assignment3.py --merge -o "${FILE_NAME}" -n "${NUM_FILES}"
+done
